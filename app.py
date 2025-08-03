f85da310926fc91ba25a2e454d6ca262fb203ff2
Alby <achore26@gmail.com>
2025-08-04 00:11:25 +0300

    Initial commit: Complete cataract detection app with pre-trained model

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, flash, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
